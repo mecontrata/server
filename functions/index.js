426e8bca0442626f44a97fde17915973561cdfd3
Rafael Franco <rafaelfr@outlook.com>
2018-09-05 17:47:23 -0300

    😫

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
+"use strict";
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const functions = require("firebase-functions");
 const firebase = require("firebase-admin");
 const cors = require("cors")({
@@ -8,15 +12,22 @@ firebase.initializeApp(functions.config().firebase);
 
 const db = firebase.firestore();
 
-exports.getApiKeys = functions.https.onRequest(async (request, response) => {
-  const docRef = await db.collection("mecontrata").doc("api");
+exports.getApiKeys = functions.https.onRequest((() => {
+  var _ref = _asyncToGenerator(function* (request, response) {
+    const docRef = yield db.collection("mecontrata").doc("api");
 
-  const result = await docRef
-    .get()
-    .then(doc => doc.data())
-    .catch(error => error);
+    const result = yield docRef.get().then(function (doc) {
+      return doc.data();
+    }).catch(function (error) {
+      return error;
+    });
 
-  return cors(request, response, () => {
-    response.status(200).send(result);
+    return cors(request, response, function () {
+      response.status(200).send(result);
+    });
   });
-});
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+})());
